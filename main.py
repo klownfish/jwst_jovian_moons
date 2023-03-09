e5cffd2f0ce42b1f14f060774a276bbc6e75fbc5
klownfish <github@clownfish.xyz>
2023-03-09 15:13:20 +0100

    big chungus

--- main.py
+++ main.py
@@ -12,23 +12,28 @@ parser = argparse.ArgumentParser(description="Tools to process JWST data related
 parser.add_argument("input", type=str)
 parser.add_argument("--glitch", action="store_true")
 parser.add_argument("--combined", action="store_true")
+parser.add_argument("--max", action="store_true")
 parser.add_argument("--single", type=int, nargs=2)
-parser.add_argument("--solar-spectrum", type=str)
 parser.add_argument("--plot", action="store_true")
 parser.add_argument("--rgb", type=str)
 parser.add_argument("--info", action="store_true")
-parser.add_argument("--albedo", action="store_true")
+parser.add_argument("--albedo", type=str)
 
 args = parser.parse_args()
 
 hdul = fits.open(args.input, memmap=False)
 
-sci_data = utils.fits_reorder_axes(hdul["SCI"].data)
-error_data = utils.fits_reorder_axes(hdul["ERR"].data)
+sci_data = np.abs(utils.fits_reorder_axes(hdul["SCI"].data))
+error_data = np.abs(utils.fits_reorder_axes(hdul["ERR"].data))
 wavelength = (hdul["SCI"].header["CRVAL3"], hdul["SCI"].header["CRVAL3"] + hdul["SCI"].header["CDELT3"] * hdul["SCI"].data.shape[0])
+
+MJy_to_W = hdul["SCI"].header["CRVAL1"] * hdul["SCI"].header["CRVAL2"]
 
 if args.info:
     hdul.info()
+    for entry in itertools.zip_longest(hdul[0].header.keys(), hdul[0].header.values()):
+        print(entry[0], entry[1])
+
     for entry in itertools.zip_longest(hdul["SCI"].header.keys(), hdul["SCI"].header.values()):
         print(entry[0], entry[1])
 
@@ -38,30 +43,33 @@ if args.single:
 
 if args.glitch:
     sci_data = utils.glitch_filter(sci_data, error_data)
+    error_data = utils.glitch_filter(error_data, error_data)
+
+if args.max:
+    (x, y) = utils.get_max(sci_data)
+    print("max pixel", x, y)
+    sci_data = utils.single_pixel(sci_data, x, y)
+    error_data = utils.single_pixel(error_data, x, y)
 
 if args.combined:
     sci_data = utils.combine_pixels(sci_data)
-    error_data = utils.combine_pixels(error_data)
+    error_data = utils.combine_errors(error_data)
 
 if args.albedo:
-    try:
-        solar_hdul = fits.open(str(pathlib.Path(__file__).parent.absolute()) + "/data/jw01538-o062_t002_nirspec_g395h-f290lp_s3d.fits", memmap=False)
-    except Exception as e:
-        print(e)
-        print('please source the file "jw01538-o062_t002_nirspec_g395h-f290lp_s3d.fits" from MAST and put it in ./data/')
+    solar_hdul = fits.open(args.albedo, memmap=False)
     solar_sci_data = utils.fits_reorder_axes(solar_hdul["SCI"].data)
     solar_error_data = utils.fits_reorder_axes(solar_hdul["ERR"].data)
 
     solar_sci_data = utils.glitch_filter(solar_sci_data, solar_error_data)
-    solar_sci_data = utils.combine_pixels(solar_sci_data)
-    for x in range(sci_data.shape[0]):
-        for y in range(sci_data.shape[1]):
-            sci_data[x, y, :] /= solar_sci_data[0, 0, :] * - 1
-    sci_data /= np.max(sci_data)
+    (x, y) = utils.get_max(solar_sci_data)
+    solar_sci_data = utils.single_pixel(solar_sci_data, x, y)
+    solar_error_data = utils.single_pixel(solar_error_data, x, y)
 
 if args.plot:
     x = np.linspace(wavelength[0], wavelength[1], num=sci_data.shape[2])
-    plt.ylim( [np.min(sci_data), np.max(sci_data)])
+    # sci_data /= np.max(np.abs(np.nanpercentile(sci_data, [3,97])))
+    # error_data /= np.max(np.abs(np.nanpercentile(error_data, [3, 97])))
+    plt.ylim(np.nanpercentile(sci_data, [5, 95]))
     plt.plot(x, sci_data[0, 0, :], marker = "o")
     plt.plot(x, error_data[0, 0, :], marker = "o")
     plt.show()
--- utils/utils.py
+++ utils/utils.py
@@ -75,11 +75,13 @@ def data_to_rgb(data, output_path):
 
     rgb_data /= np.max(rgb_data)
     rgb_data = np.clip(rgb_data, 0, 1)
+    rgb_data[np.isnan(rgb_data)] = 0
     output_img = Image.new("RGB", rgb_data.shape[:2])
     for x in range(rgb_data.shape[0]):
         for y in range(rgb_data.shape[1]):
             value = rgb_data[x, y]
             value = np.floor(value * 255)
+            print(value)
             output_img.putpixel((x, y), tuple(value.astype(int)))
     output_img.save(output_path)
 
@@ -103,17 +105,64 @@ def fits_reorder_axes(data):
 #                         output[x, y, z] = data[x, y, z]
 #     return output
 
+def filter_full_nan(data):
+    output = np.empty_like(data)
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+            if np.nansum(data[x, y, :]) == 0:
+                output[x, y, :] = 0
+            else:
+                output[x, y, :] = data[x, y, :]
+
+    return output
+
+def get_max(data):
+    test = np.nanmedian(data, axis=2)
+    found_max = 0
+    max_x = 0
+    max_y = 0
+    for x in range(test.shape[0]):
+        for y in range(test.shape[1]):
+            if np.isnan(test[x,y]):
+                continue
+            if test[x,y] > found_max:
+                (max_x, max_y) = (x, y)
+                found_max = test[x,y]
+    return (max_x, max_y)
+
 def glitch_filter(data, error):
-    [_, cutoff] = np.percentile(data, [0, 90])
-    mask = (error == 0) | (error > abs(cutoff) * 3)
+    [_, cutoff] = np.percentile(error, [0, 90])
+    mask = (error == 0) | (error > abs(cutoff) * 20)
     nans = np.full(data.shape, np.nan)
-    nans[..., 0] = data[..., 0]
-    return bn.push(np.where(mask, nans, data))
+    # nans[..., 0] = data[..., 0]
+    # return bn.push(np.where(mask, nans, data))
+    output = np.where(mask, nans, data)
+    output = filter_full_nan(output)
+    return output
 
 def combine_pixels(data):
-    output = np.ndarray(shape=(1, 1, data.shape[2]))
+    energy = np.nanmean(data, axis=2)
+    mean = np.nanmean(energy)
+    good_pixels = np.empty_like(data)
+
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+            if energy[x, y] < mean:
+                good_pixels[x, y, :] = np.nan
+            else:
+                good_pixels[x, y, :] = data[x, y, :]
+    output = np.ndarray(shape=(1, 1, good_pixels.shape[2]))
     for z in range(data.shape[2]):
-        output[0, 0, z] = np.mean(data[:, :, z])
+        output[0, 0, z] = np.nanmean(good_pixels[:, :, z])
+    return output
+
+def combine_errors(data):
+    # data = np.where(data == 0, np.nanmean(data), data)
+    output = np.ndarray(shape=(1, 1, data.shape[2]))
+    nans = np.full(data.shape, np.nan)
+    output = np.where(data == 0, nans, data)
+    for z in range(data.shape[2]):
+        output[0, 0, z] = np.nanmean(data[:, :, z])
     return output
 
 def single_pixel(data, x, y):
